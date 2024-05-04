3b6189025940e0e9347684cd8a17e5a7e57fd82a
cwesly <cauawesly@outlook.com>
2024-05-04 16:05:40 -0300

    feat: creat title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Button from "./components/button"
 import Input from "./components/input"
+import Title from "./components/title/title"
 import "./style.css"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   }
   return (
     <div className="main">
+      <Title>Tarefas</Title>
       <Input type="text" placeholder="Adicione uma tarefa" />
       <Button text="Adicionar" type="add" handleClick={handleAdd} />
       <Button text="Limpar" type="remove" handleClick={handleRemove} />
---
+++ src/components/title/title.tsx
@@ -0,0 +1,9 @@
+import "./style.css"
+type TitleProps = {
+    children: string
+}
+
+const Title = ({children}: TitleProps) => {
+    return <h1 className="title">{children}</h1>
+}
+export default Title
