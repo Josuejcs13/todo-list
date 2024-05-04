eefd5f8b8b52c4fe543504ebfd222730ab7aa3cf
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-05-04 14:56:40 -0300

    chore: initial config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "./components/button";
+
+function App() {
+  return (
+    <div>
+      <Button text="Adicionar" type="add"></Button>
+      <Button text="Limpar" type="remove"></Button>
+    </div>
+  );
+}
+
+export default App;
