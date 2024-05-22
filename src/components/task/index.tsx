67f5716f39f1b3a76f01c99a4283a3df59c0c135
josuejcs13 <josuejcs13@gmail.com>
2024-05-21 21:29:01 -0400

    feat: add edition component SelectType

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,22 +1,34 @@
-import { useState } from "react";
-import "./style.css";
-import { CiCircleRemove, CiEdit, CiCircleCheck } from "react-icons/ci";
-import Input from "../input";
+import { useState } from "react"
+import "./style.css"
+import { CiCircleRemove, CiEdit, CiCircleCheck } from "react-icons/ci"
+import Input from "../input"
+import SelectType from "../selectType"
+import { Category } from "../../types"
 
 type TaskProps = {
-  text: string;
-  id: number;
-  handleRemoveTask: (id: number) => void;
-  handleEditTask: (editedText: string, id: number) => void;
-};
+  text: string
+  id: number
+  handleRemoveTask: (id: number) => void
+  handleEditTask: (editedText: string, id: number, category: Category) => void
+  category: Category
+  setCategory: (category: Category) => void
+}
 
-const Task = ({ text, id, handleRemoveTask, handleEditTask }: TaskProps) => {
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [taskEdited, setTaskEdited] = useState<string>("");
+const Task = ({
+  text,
+  id,
+  handleRemoveTask,
+  handleEditTask,
+  category,
+}: TaskProps) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [taskEdited, setTaskEdited] = useState<string>("")
+  const [typeEdited, setTypeEdited] = useState<Category>(category)
+
   const handleEdit = () => {
-    setIsEditing(false);
-    handleEditTask(taskEdited, id);
-  };
+    setIsEditing(false)
+    handleEditTask(taskEdited, id, typeEdited as Category)
+  }
 
   return (
     <div className="task">
@@ -28,6 +40,7 @@ const Task = ({ text, id, handleRemoveTask, handleEditTask }: TaskProps) => {
             value={taskEdited}
             setValue={setTaskEdited}
           />
+          <SelectType category={typeEdited} setCategory={setTypeEdited} />
           <button onClick={handleEdit}>
             <CiCircleCheck />
           </button>
@@ -40,11 +53,12 @@ const Task = ({ text, id, handleRemoveTask, handleEditTask }: TaskProps) => {
           <CiEdit />
         </button>
       )}
+      <p>{category}</p>
       <button onClick={() => handleRemoveTask(id)}>
         <CiCircleRemove />
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Task;
+export default Task
